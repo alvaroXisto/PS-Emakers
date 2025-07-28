8fc451dff410b4ebed35b8577efc0e6ddc2041df
Alvaro Xisto <134233158+alvaroXisto@users.noreply.github.com>
2025-07-28 10:44:00 -0300

    Add files via upload

---
+++ api-rota-back/src/main/java/com/alvaro/api_rota_back/ApiRotaBackApplication.java
@@ -0,0 +1,13 @@
+package com.alvaro.api_rota_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRotaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRotaBackApplication.class, args);
+	}
+
+}
---
+++ api-rota-back/src/test/java/com/alvaro/api_rota_back/ApiRotaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvaro.api_rota_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRotaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
